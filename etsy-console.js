756e65601934d99f2fa5a81e2b675cad07b6017f
Selim <selim_koyuncu@hotmail.com>
2024-03-14 16:52:14 +0300

    eng stable ver.

--- etsy-console.js
+++ etsy-console.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Etsy Listelenme Tarihi ve Favori Sayısı
+// @name         Etsy Listing Date and Favorite Count
 // @namespace    https://github.com/cengaver
 // @version      0.211
-// @description  Etsy'de bir ürünün listelenme tarihini ve favori sayısını gösterir.
+// @description  Displays the listing date and favorite count of a product on Etsy.
 // @author       Cengaver
 // @match        https://www.etsy.com/*
 // @grant        GM_addStyle
@@ -12,16 +12,16 @@
 (function() {
     'use strict';
 
-    // Tarihi ve favori sayısını al
+    // Extract listing date and favorite count from HTML content
     function getInfoFromHTML(htmlContent) {
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlContent, 'text/html');
 
-        // Listelenme tarihini bul
+        // Find the listing date
         const listingDateElement = doc.querySelector('.wt-pr-xs-2.wt-text-caption');
         const listingDate = listingDateElement ? listingDateElement.textContent.trim().replace("Listed on ", "") : '';
 
-        // Favori sayısını bul
+        // Find the favorite count
         const favoriteCountElement = doc.querySelector('.wt-text-link[href*="/favoriters"]');
         let favoriteCount = 0;
         if (favoriteCountElement) {
@@ -35,66 +35,63 @@
         return { listingDate, favoriteCount };
     }
 
-    // Tarihi bugünden kaç gün önce olduğunu hesapla
+    // Calculate days ago from a given date string
     function daysAgoFromDate(dateString) {
-        // Verilen tarih stringini tarihe dönüştür
+        // Convert the given date string to a date object
         const listingDate = new Date(dateString);
-        // Bugünün tarihini al
+        // Get today's date
         const today = new Date();
-        // Tarihler arasındaki farkı hesapla ve gün cinsinden dön
+        // Calculate the difference between the dates and return in days
         const differenceInTime = today.getTime() - listingDate.getTime();
         const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
         return differenceInDays;
     }
 
-    // Dize içindeki etiketleri teke indirgeme
+    // Remove duplicate tags from a string
     function removeDuplicateTags(tagString) {
-        // Etiketleri virgülle ayırarak bir dizi oluştur
+        // Split the tag string by comma to create an array
         const tagArray = tagString.split(',').map(tag => tag.trim());
 
-        // Dizideki benzersiz öğeleri al
+        // Get unique elements from the array
         const uniqueTags = [...new Set(tagArray)];
 
-        // Benzersiz öğeleri birleştirerek tekilleştirilmiş bir dize elde et
+        // Join unique elements to get a string with no duplicates
         const uniqueTagString = uniqueTags.join(', ');
 
         return uniqueTagString;
     }
 
-    // Başlığı düzenle
-function titleReplace(titleString) {
-    let titleReplaced = titleString ? titleString.trim() : '';
-    titleReplaced = titleReplaced.replaceAll(/T-Shirt|Cutting File|Cut File for Cricut|DIY Projects|Design|Vector|Eps|Jpg|Dxf|Print|Pdf|Svg|Png|File|Transparent|Prints|Sublimation|Clipart|Cutting Machine|Instant Download|Sublimate|Image|Download|Downloads|Clip Art|Designs|Cricut|Silhouette|Digital|Canvas|Surfaces|Stencils|Templates|Transfers|Shirts/gi, 'Shirt');
-    titleReplaced = titleReplaced.replaceAll(/&/g, '');
-    titleReplaced = titleReplaced.replaceAll(/\|/g, ',');
-    titleReplaced = titleReplaced.replaceAll(/\s+/g, ' ');
-    let regex = /Shirt Shirt/gi;
-    let regex2 = /Shirt, Shirt/gi;
-    let regex3 = /Shirt & Shirt/gi;
-    let regex4 = /, Shirt,/gi;
-    while (titleReplaced.match(regex2)) {
-        titleReplaced = titleReplaced.replaceAll(regex2, 'Shirt').trim();
+    // Replace specific terms in a title
+    function titleReplace(titleString) {
+        let titleReplaced = titleString ? titleString.trim() : '';
+        titleReplaced = titleReplaced.replaceAll(/T-Shirt|Cutting File|Cut File for Cricut|DIY Projects|Design|Vector|Eps|Jpg|Dxf|Print|Pdf|Svg|Png|File|Transparent|Prints|Sublimation|Clipart|Cutting Machine|Instant Download|Sublimate|Image|Download|Downloads|Clip Art|Designs|Cricut|Silhouette|Digital|Canvas|Surfaces|Stencils|Templates|Transfers|Shirts/gi, 'Shirt');
+        titleReplaced = titleReplaced.replaceAll(/&/g, '');
+        titleReplaced = titleReplaced.replaceAll(/\|/g, ',');
+        titleReplaced = titleReplaced.replaceAll(/\s+/g, ' ');
+        let regex = /Shirt Shirt/gi;
+        let regex2 = /Shirt, Shirt/gi;
+        let regex3 = /Shirt & Shirt/gi;
+        let regex4 = /, Shirt,/gi;
+        while (titleReplaced.match(regex2)) {
+            titleReplaced = titleReplaced.replaceAll(regex2, 'Shirt').trim();
+        }
+        while (titleReplaced.match(regex3)) {
+            titleReplaced = titleReplaced.replaceAll(regex3, 'Shirt').trim();
+        }
+        while (titleReplaced.match(regex)) {
+            titleReplaced = titleReplaced.replaceAll(regex, 'Shirt').trim();
+        }
+        while (titleReplaced.match(regex4)) {
+            titleReplaced = titleReplaced.replaceAll(regex4, '').trim();
+        }
+        return removeDuplicateTags(titleReplaced);
     }
-    while (titleReplaced.match(regex3)) {
-        titleReplaced = titleReplaced.replaceAll(regex3, 'Shirt').trim();
-    }
-    while (titleReplaced.match(regex)) {
-        titleReplaced = titleReplaced.replaceAll(regex, 'Shirt').trim();
-    }
-    while (titleReplaced.match(regex4)) {
-        titleReplaced = titleReplaced.replaceAll(regex4, '').trim();
-    }
-    return removeDuplicateTags(titleReplaced);
-}
 
-    // HTML içeriğindeki liste öğelerinin metin içeriklerini bir araya getirir
+    // Concatenate text content of list items in HTML
     function concatenateListItems(tagContent) {
-        // Metin içeriklerini bir araya getir
         let concatenatedText = '';
         tagContent.forEach((item, index) => {
-            // Her öğenin metnini al ve virgülle birleştir
             concatenatedText += item.textContent.trim();
-            // Son öğe değilse virgül ekle
             if (index < tagContent.length - 1) {
                 concatenatedText += ', ';
             }
@@ -102,28 +99,28 @@ function titleReplace(titleString) {
 
         return concatenatedText;
     }
-    // belli metni panoya kopyala
+
+    // Copy a specified text to clipboard
     function copyText(textToCopy) {
         navigator.clipboard.writeText(textToCopy)
         .then(() => {
-            console.log('Metin başarıyla panoya kopyalandı:', textToCopy);
+            console.log('Text copied to clipboard:', textToCopy);
         })
         .catch(err => {
-            console.error('Metin kopyalanırken bir hata oluştu:', err);
+            console.error('Error copying text:', err);
         });
     }
-    
-    function virgulluSayiCevir(virgulluSayi) {
-        // Virgüllü sayıyı normal sayıya çevir
-        return parseFloat(virgulluSayi.replace(',', '.'));
+
+    // Convert comma-separated number to normal number
+    function convertCommaSeparatedNumber(commaSeparatedNumber) {
+        return parseFloat(commaSeparatedNumber.replace(',', '.'));
     }
-    // Ürün sayfasında mı kontrol ediyoruz
+
+    // Check if we are on a product page
     if (window.location.href.indexOf("etsy.com/listing/") !== -1) {
-        // Listelenme tarihini bulmak için gerekli sınıf
         const listingDateElement = document.querySelector('.wt-pr-xs-2.wt-text-caption');
         const listingDate = listingDateElement ? listingDateElement.textContent.trim().replace("Listed on ", "") : '';
 
-        // Favori sayısını bulmak için gerekli sınıf
         const favoriteCountElement = document.querySelector('.wt-text-link[href*="/favoriters"]');
         let favoriteCount = 0;
         if (favoriteCountElement) {
@@ -134,15 +131,12 @@ function titleReplace(titleString) {
             }
         }
 
-        // Başlık ve etiketleri al
         const listingTitleElement = document.querySelector('#listing-page-cart > div.wt-mb-xs-1 > h1');
         const listingTitle = titleReplace(listingTitleElement.textContent.trim());
 
-        // Liste öğelerini al
         const listItems = document.querySelectorAll('.tag-card-title');
         const concatenatedTextValue = concatenateListItems(listItems);
 
-        // İkinci etiketleri al
         let conText = "";
         const tagContainer = document.querySelector('.wt-action-group.wt-list-inline.wt-mb-xs-2');
         if (!tagContainer) {
@@ -152,18 +146,12 @@ function titleReplace(titleString) {
             conText = titleReplace(concatenatedTextValue + "," + tagElements);
         }
 
-        // Başlık ve etiketleri sayfaya ekle
-        listingTitleElement.innerHTML = `
-            ${listingTitleElement.textContent}<br><hr><div class="wt-bg-turquoise-tint wt-text-gray wt-text-caption wt-pt-xs-1 wt-pb-xs-1">${listingTitle} <br><hr>${conText}</div>
-        `;
-
-        // Eğer listelenme tarihi ve favori sayısı bulunduysa
         if (listingDate && favoriteCount) {
-            console.log("Listelenme Tarihi: " + listingDate);
-            console.log("Favori Sayısı: " + favoriteCount);
-            console.log("Başlık: " + listingTitle);
-            console.log("Etiket: " + concatenatedTextValue);
-            //console.log("Etiket2: " + tagElements);
+            console.log("Listing Date: " + listingDate);
+            console.log("Favorite Count: " + favoriteCount);
+            console.log("Title: " + listingTitle);
+            console.log("Tags: " + concatenatedTextValue);
+
             const avrage = favoriteCount / daysAgoFromDate(listingDate);
             if (favoriteCount > 50) {
                 favoriteCount = "❤️" + favoriteCount;
@@ -177,24 +165,19 @@ function titleReplace(titleString) {
             const reviewItemElement = document.querySelector(' #same-listing-reviews-tab > span');
             let review ="";
             if (reviewItemElement!== null){
-                var reviewCount = virgulluSayiCevir(reviewItemElement.textContent.trim());
+                var reviewCount = convertCommaSeparatedNumber(reviewItemElement.textContent.trim());
             if (reviewCount > 10) {
                 reviewCount = "★" + reviewCount;
             }
                 review = '<p style="margin: 0;">Rev : '+reviewCount+' </p>'
                 console.log("ReviewItem: " + reviewCount);
             }
-            // İlgili script etiketini seç
             const script = document.querySelector('script[type="application/ld+json"]');
-
-            // Script etiketinin içeriğini al
             const jsonData = JSON.parse(script.textContent.trim());
-
-            // Alınan JSON verisini konsola yazdır
             console.log(jsonData);
             const eligibleQuantity = jsonData.offers.eligibleQuantity;
             const offerCount = jsonData.offers.offerCount;
-            console.log("eligibleQuantity:", eligibleQuantity);
+            console.log("Eligible Quantity:", eligibleQuantity);
             let sales ="";
             if (eligibleQuantity=== undefined){ eligibleQuantity = offerCount}
             if (eligibleQuantity!== undefined){
@@ -203,9 +186,9 @@ function titleReplace(titleString) {
                 salesCount = "★" + salesCount;
             }
                 sales = '<p style="margin: 0;">Sat : '+salesCount+' </p>'
-                console.log("salesCount: " + salesCount);
+                console.log("Sales Count: " + salesCount);
             }
-            // Baloncuk oluşturma
+
             const balloonDiv = document.createElement("div");
             balloonDiv.setAttribute("id", "etsyInfoBalloon");
             balloonDiv.innerHTML = `
@@ -215,23 +198,20 @@ function titleReplace(titleString) {
                      ${sales}
                     <p style="margin: 0;">Fav : ${favoriteCount}</p>
                     <p style="margin: 0;">${listingDate}</p>
-                    <p style="margin: 0;">Gün : ${daysAgoFromDate(listingDate)}</p>
+                    <p style="margin: 0;">Days Ago : ${daysAgoFromDate(listingDate)}</p>
                     <p style="margin: 0;">Avg : ${parseFloat(avrage.toFixed(1))}</p>
-                    <button id="copy">Kopyala</button>
+                    <button id="copy">Copy</button>
                 </div>
             `;
-            // Baloncugu sayfaya ekleme
             document.body.appendChild(balloonDiv);
-            // Input elementine erişim
+
             const inputElement = document.querySelector('input[name="listing_id"]');
-            // Değerini alma
             const listingId = inputElement.value;
-            // Değeri konsola yazdırma
             console.log("Listing ID:", listingId);
 
             document.getElementById("copy").onclick = function(){
-                navigator.clipboard.writeText("Listing ID:"+listingId+"\n"+listingTitle+"\n"+conText)
-                console.log("Metin başarıyla panoya kopyalandı:\nListing ID:"+listingId+"\n"+listingTitle+"\n"+conText);
+                copyText("Listing ID:"+listingId+"\n"+listingTitle+"\n"+conText)
+                console.log("Text copied to clipboard:\nListing ID:"+listingId+"\n"+listingTitle+"\n"+conText);
             }
         }
     }
